60bb234e0c539b389cda8fa22289e523cc831360
2010vertika <85236833+2010vertika@users.noreply.github.com>
2021-10-13 12:42:22 +0530

    Add files via upload

---
+++ Kwitter_room.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDdnBTZMAO7AGhk5m5qbIhZuYg03D8gm_g",
+  authDomain: "c-94-project-5d74e.firebaseapp.com",
+  databaseURL: "https://c-94-project-5d74e-default-rtdb.firebaseio.com",
+  projectId: "c-94-project-5d74e",
+  storageBucket: "c-94-project-5d74e.appspot.com",
+  messagingSenderId: "1089003919662",
+  appId: "1:1089003919662:web:f61ab20bd2f7b150c602bf"
+};
+
+const app = initializeApp(firebaseConfig);
+
+
+  
